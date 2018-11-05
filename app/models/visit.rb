d5ddc2db1b4d002048fb06c647a66a913561fb57
Geovany Gameros <geovanygameros@gmail.com>
2018-11-05 10:01:41 -0700

    Se administra espacio en grid de visitas

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -63,19 +63,19 @@ class Visit < ApplicationRecord
 
   def send_requested_email
     VisitsMailer.new_visit(self).deliver_later
-    puts "[SOLICITUD] Se notificará a #{self.resp_email} sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
+    puts "[#{DateTime.now.to_s}][SOLICITUD] Se notificará a #{self.resp_email} sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
 
     # También se envía correo al administrador para que le dé seguimiento a la visita
     User.where(user_type:User::ADMIN).each do |user|
       VisitsMailer.new_visit_admin(self, user.email).deliver_later
-      puts "[NOTIFICACIÓN] Se notificará al administrador sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
+      puts "[#{DateTime.now.to_s}][NOTIFICACIÓN] Se notificará al administrador sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
     end
 
     # También se envía correo al asistente del departamento para que le dé seguimiento a la visita
     User.where(user_type:User::ASSISTANT).each do |assistant|
       if assistant.departments.include? self.department_id
         VisitsMailer.new_visit_assistant(self, assistant.email).deliver_later
-        puts "[NOTIFICACIÓN] Se notificará a #{assistant.name} sobre visita de #{self.institution} (id: #{self.id})"
+        puts "[#{DateTime.now.to_s}][NOTIFICACIÓN] Se notificará a #{assistant.name} sobre visita de #{self.institution} (id: #{self.id})"
       end
     end
 
@@ -85,28 +85,37 @@ class Visit < ApplicationRecord
   def send_pre_approved_email
     User.where(user_type:User::ADMIN).each do |user|
       VisitsMailer.visit_pre_approved(self, user.email).deliver_later
-      puts "[PRE-APROBADA] Se notificará al administrador sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
+      puts "[#{DateTime.now.to_s}][PRE-APROBADA] Se notificará al administrador sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
     end
   end
 
   def send_approved_email
     VisitsMailer.visit_approved(self).deliver_later
-    puts "[APROBADA] Se notificará a #{self.resp_email} sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
+    puts "[#{DateTime.now.to_s}][APROBADA] Se notificará a #{self.resp_email} sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
   end
 
   def send_confirmed_email
     VisitsMailer.visit_confirmed(self).deliver_later
-    puts "[CONFIRMADA] Se notificará a #{self.resp_email} sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
+    puts "[#{DateTime.now.to_s}][CONFIRMADA] Se notificará a #{self.resp_email} sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
 
     User.where(user_type:User::ADMIN).each do |user|
       VisitsMailer.visit_confirmed_admin(self, user.email).deliver_later
-      puts "[CONFIRMADA] Se notificará al administrador sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
+      puts "[#{DateTime.now.to_s}][CONFIRMADA] Se notificará al administrador sobre visita de #{self.institution} a #{self.department.name} (id: #{self.id})"
     end
 
     User.where(user_type:User::ASSISTANT).each do |assistant|
       if assistant.departments.include? self.department_id
         VisitsMailer.visit_confirmed_admin(self, assistant.email).deliver_later
-        puts "[CONFIRMADA] Se notificará a #{assistant.name} sobre visita de #{self.institution} (id: #{self.id})"
+        puts "[#{DateTime.now.to_s}][CONFIRMADA] Se notificará a #{assistant.name} sobre visita de #{self.institution} (id: #{self.id})"
+      end
+    end
+  end
+
+  def send_confirm_email_technician
+    self.room_visits.where(status: RoomVisit::SCHEDULED).each do |room_visit|
+      if room_visit.persona.cuenta_cimav
+        VisitsMailer.visit_confirmed_technician(room_visit).deliver_later
+        puts "[#{DateTime.now.to_s}][CONFIRMAR VISITA] Se notificará a #{room_visit.persona.nombre} sobre visita de #{room_visit.visit.institution} (id: #{self.id})"
       end
     end
   end
--- lib/tasks/room_visits.rake
+++ lib/tasks/room_visits.rake
@@ -1,13 +1,12 @@
 #coding:utf-8
 desc "Manda un email al técnico para confirmar la visita al laboratorio"
 task :confirm_room_visits => :environment do
-  today = Date.today
   # Se verifican las visitas a laboratorio con estatus de agendada y que la visita está confirmada o finalizada y que ya pasaron
   room_visits = RoomVisit.joins(:visit).where('visits.date < ? AND visits.status IN(?)', Date.today, [Visit::CONFIRMED, Visit::FINISHED]).where(status: RoomVisit::SCHEDULED)
   room_visits.each do |room_visit|
     if room_visit.persona.cuenta_cimav
-      VisitsMailer.room_visit_confirm(room_visit).deliver_later
-      puts "[CONFIRMAR VISITA] Se notificará a #{room_visit.persona.nombre} sobre visita de #{room_visit.visit.institution} (id: #{visit.id})"
+      VisitsMailer.confirm_room_visit(room_visit).deliver_later
+      puts "[#{DateTime.now.to_s}][CONFIRMAR VISITA] Se notificará a #{room_visit.persona.nombre} sobre visita de #{room_visit.visit.institution} (id: #{room_visit.id})"
     end
   end
 end
@@ -20,7 +19,7 @@ task :reminder_confirm_visit => :environment do
   visits = Visit.where('status = ? AND date BETWEEN ? AND ?', Visit::APPROVED, today + 3.days, today + 5.days)
   visits.each do |visit|
     VisitsMailer.visit_reminder_confirm(visit).deliver_later
-    puts "[RECORDAR CONFIRMAR VISITA] Se recordará a #{visit.resp_name} confirmar visita de #{visit.institution} (id: #{visit.id})"
+    puts "[#{DateTime.now.to_s}][RECORDAR CONFIRMAR VISITA] Se recordará a #{visit.resp_name} confirmar visita de #{visit.institution} (id: #{visit.id})"
   end
 end
 
@@ -31,7 +30,7 @@ task :cancel_unconfirmed_visits => :environment do
   visits = Visit.where('status = ? AND date < ?', Visit::APPROVED, today + 3.days)
   visits.each do |visit|
     if visit.update(status: Visit::CANCELED)
-      puts "[VISITA CANCELADA] Al no haber confirmación, se cancela visita de #{visit.institution} (id: #{visit.id})"
+      puts "[#{DateTime.now.to_s}][VISITA CANCELADA] Al no haber confirmación, se cancela visita de #{visit.institution} (id: #{visit.id})"
     end
   end
 end
@@ -43,7 +42,7 @@ task :reminder_pending_requests => :environment do
     pending_requests = Visit.where(status: Visit::REQUESTED).where(department_id: assistant.departments).size
     if pending_requests > 0
       VisitsMailer.reminder_pending_requests(pending_requests, assistant.email).deliver_later
-      puts "[RECORDAR SOLICITUDES] Se recordará a #{assistant.name} sobre #{pending_requests} solicitudes pendientes en sus departamentos"
+      puts "[#{DateTime.now.to_s}][RECORDAR SOLICITUDES] Se recordará a #{assistant.name} sobre #{pending_requests} solicitudes pendientes en sus departamentos"
     end
   end
 end
